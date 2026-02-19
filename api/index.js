388907ad68b907e86a98169611c4a4ffc3965c9c
Mael Gruand <maelg396@gmail.com>
2026-02-19 10:26:16 +0100

    Creando backend base

--- api/index.js
+++ api/index.js
@@ -3,12 +3,11 @@ const cors = require('cors')
 const port = 8080
 
 const app = express()
-app.use(cors)
+app.use(cors())
+app.use(express.json())
 
-app.get('/', req, res => {
+app.get('/', (req, res) => {
     res.json({ message: 'Message de prueba desde el backend' })
 })
 
-app.listen(port, ()=>{
-    console.log(`App listen on port ${port}`)
-})
+module.exports = app
